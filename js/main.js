892d825d95bc09c2f3a86151aeb47b778a934568
Marius <marius@example.com>
2022-07-25 13:37:50 +0300

    atvaizduoti

--- js/main.js
+++ js/main.js
@@ -4,3 +4,5 @@ console.log('main.js');
 const nums = [-50, 14, 0, 24, 100, -45];
 
 // isrikiuoti masyva a-z ir nuo z-a
+
+// atvaizduoti <div id="app"></div> orginala, az ir za versijas atsikruose <p></p> taguose
