3eb8c7e7370e54eefb6f690896980d6ad265e9a3
Marius <marius@example.com>
2022-07-25 13:54:28 +0300

    printArrInApp uzd

--- js/main.js
+++ js/main.js
@@ -2,7 +2,22 @@
 console.log('main.js');
 
 const nums = [-50, 14, 0, 24, 100, -45];
+printArrInApp('orginalas', nums);
 
 // isrikiuoti masyva a-z ir nuo z-a
+const sortedAz = nums.sort((a, b) => a - b);
+console.log('sortedAz ===', sortedAz);
+const sortedZa = nums.sort((a, b) => b - a);
+console.log('sortedZa ===', sortedZa);
+console.log({ sortedAz, nums });
 
 // atvaizduoti <div id="app"></div> orginala, az ir za versijas atsikruose <p></p> taguose
+
+function printArrInApp(title, arr) {
+  // title = orginalas | AZ | ZA
+  // nusitaikyti i app
+  // sukurti paragrafa
+  // sudeti visus el is arr i parafa (butu gerai be el.textContent +=)
+}
+
+// <button>Sort</button> paspaudus mygtuka isrikiuojam orginala nuo A iki Z
